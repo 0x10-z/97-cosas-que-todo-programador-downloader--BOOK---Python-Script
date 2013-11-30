12bed606454b13f1c94f3af33760d86f0041d6f9
Iker Ocio Zuazo <ikerocio@yahoo.es>
2013-11-30 03:14:09 +0100

    change reportlab lib for xhtml2pdf to solve <li>, <pre> and <code> tags problems

--- 97cosas_downloader.py
+++ 97cosas_downloader.py
@@ -14,63 +14,193 @@
 #
 
 import requests
+import os, sys
+from pyPdf import PdfFileWriter, PdfFileReader
 import lxml.html, lxml.cssselect
-import time
+from time import sleep
 from urlparse import urljoin
 import urllib2
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, PageBreak
+from xhtml2pdf import pisa             # import python module
 
-#init = raw_input("Press enter to start downloading from\nhttp://97cosas.com/programador/")
 
-output = "/home/ocioz/Escritorio/97 cosas que todo programador.pdf"    # set your ouput path for file
-url_base = 'http://97cosas.com/programador/'    # url where every chapter is
-request = requests.get(url_base)                # 
-source = request.content    # get request source
-html = lxml.html.fromstring(source)    # get html code
+output_pdf_path = "/home/ocioz/Escritorio/97 cosas que todo programador.pdf"    # set your ouput path for file
+url_base = 'http://97cosas.com/programador/'    # url where each chapter will be
+temp_path = "/home/ocioz/Escritorio/temp"    # set temp path. If not exists, script create automatically
 
+
+# Utility function
+def convertHtmlToPdf(sourceHtml, output_pdfFilename):
+    # open output_pdf file for writing (truncated binary)
+    resultFile = open(output_pdfFilename, "w+b")
+
+    # convert HTML to PDF
+    pisaStatus = pisa.CreatePDF(
+            sourceHtml,                # the HTML to convert
+            dest=resultFile)           # file handle to recieve result
+
+    # close output_pdf file
+    resultFile.close()                 # close output_pdf file
+
+    # return True on success and False on errors
+    return pisaStatus.err
+
+
+
+
+def concatenate_pdf(book_title, num_chapters):
+        fileList = os.listdir(temp_path)
+        
+        
+        print"Uniendo pdfs..."
+        output_pdf = PdfFileWriter()
+
+        for i in range (1,num_chapters):
+            f=open(temp_path+"/"+str(i)+"x97.pdf", "rb")
+            num_pages=PdfFileReader(f).getNumPages()
+            if num_pages==0:
+                pdfOne = PdfFileReader(f).getPage(0)
+                output_pdf.addPage(pdfOne)
+            else:
+                for a in range (0,num_pages):
+                    pdfOne = PdfFileReader(f).getPage(a)
+                    output_pdf.addPage(pdfOne)
+
+        print file
+        output_pdfStream = file(r""+book_title, "wb")
+        output_pdf.write(output_pdfStream)
+        output_pdfStream.close()
+        print "Union finalizada\nTienes el archivo creado en..."+output_pdf_path
+        for i in range(1,num_chapters+1):
+            print "borrando... capitulo: "+str(i)
+            os.remove(temp_path+"/"+str(i)+"x97.pdf")
+
+
+
+def get_page_html(url):
+    "return HtmlElement of url"
+    request = requests.get(url)
+    source = request.content
+    return lxml.html.fromstring(source)
+
+"""
+******************
+HTML and CSS style
+******************
+"""
+
+body_style="""
+font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+font-size: 14px;
+line-height: 20px;
+color: #333333;
+"""
+pre_style="""
+display: block;
+padding: 9.5px;
+margin: 0 0 10px;
+font-size: 13px;
+line-height: 20px;
+word-break: break-all;
+word-wrap: break-word;
+white-space: pre-wrap;
+background-color: #f5f5f5;
+background-color: #772953;
+color:#FFFFFF;
+border-radius: 4px;
+"""
+code_style="""
+font-family: Monaco, Menlo, Consolas, "Courier New", monospace;
+font-size: 12px;
+font-weight:bold;
+white-space:pre
+"""
+
+"""
+*************************
+end of HTML and CSS style
+*************************
+"""
+
+
+init = raw_input("Press enter to start downloading from\nhttp://97cosas.com/programador/\n(ONLY TEST)If you want concatenate PDFs files, press 1 and enter(ONLY TEST)\n")
+
+if init == "1":
+    concatenate_pdf("97 cosas que todo programador", 97)
+    break
+
+if not os.path.exists(temp_path):
+    os.makedirs(temp_path)
+
+
+
+html = get_page_html(url_base)    # get html code
+sourceHtml="<div style='font-size:12px'>"
 
 j = 1
-
-# PDF STYLESHEET
-style_sheet=getSampleStyleSheet()    # pdf style sheet
-style_sheet.fontSize=50    # this doesn't work
-story=[]    # story object where we are going to save every chapter for then export to pdf
-h1 = style_sheet['Heading1']    
-style=style_sheet['BodyText']
-
+max_num_chapter = 0
 for i in html.cssselect('div.span12')[0].cssselect('ol')[0].cssselect('li'):    # every chapter iterator
-    
-    chapter_url = urljoin(url_base, i.cssselect('a')[0].get('href'))    # chapter's url
-    chapter_title = str(j) +" - "+ i.cssselect('a')[0].text_content()    # chapter's title
-    
+    try:
+        chapter_url = urljoin(url_base, i.cssselect('a')[0].get('href'))
+        chapter_title = str(j) +" - "+ i.cssselect('a')[0].text_content()    # chapter's title
+        html2 = get_page_html(chapter_url)     # chapter's html
 
-    chapter_content = requests.get(chapter_url).content    
-    html2 = lxml.html.fromstring(chapter_content)     # chapter's html
-    chapter_author = html2.cssselect('div.span12')[0].cssselect('small')[0].text_content() # chapter's real author
+        for x in html2.cssselect('div.span12')[0]:    # paragrap iterator
+            if not (x.cssselect('ul.dropdown-menu') or "Leer contr" in x.text_content()):    # Do not want these elements
+        		sourceHtml = sourceHtml + lxml.html.tostring(x)    # Convert HtmlElement to string and add
 
-    # put chapter title into pdf
-    h1.pageBreakBefore=0      # 0 page break
-    h1.keepWithNext=1
-    p1 = Paragraph(chapter_title, h1)
-    story.append(p1)
-    story.append(Spacer(0,15)) # title and body spacer
-   
-    for x in html2.cssselect('div.span12')[0].cssselect('p'):    # paragrap iterator
-        if not "Leer contr" in x.text_content():
-            # print x.text_content()
-            p2=Paragraph(x.text_content(),style)    # set paragraph into storyboard
-            story.append(p2)    # set storyboard into pdf
-            time.sleep(.5)
-            
+        print "Chapter "+str(j)+" created -",chapter_url
+        sleep(.2)
 
-    print "Chapter "+str(j)+" created"
-    j+=1
-    story.append(PageBreak())    
+        " Edit html with correct styles to create pdf file "
+        sourceHtml = sourceHtml.replace("<body", "<body style='"+body_style+"'")
+        sourceHtml = sourceHtml.replace("<pre", "<pre style='"+pre_style+"'")
+        sourceHtml = sourceHtml.replace("<code", "<code style='"+code_style+"'")
+        sourceHtml = sourceHtml.replace("<h2", "<h2 style='font-size:31.5px'")
+        sourceHtml = sourceHtml.replace("<small", "<small style='font-weight:normal;font-size:15px;color:#999999'")
+        sourceHtml = sourceHtml+"</div></body></html>"
 
-pdf=SimpleDocTemplate(output,pagesize=A4,showBoundary=1)    # create a pdf template
-pdf.build(story)
+        output_pdfFilename = str(j)+"x97.pdf" # Temp pdf file
+        j+=1
+        pisa.showLogging()
+        convertHtmlToPdf(sourceHtml, temp_path+"/"+output_pdfFilename)
+        sourceHtml = ""    # Clear html text for another chapter
+
+    except requests.exceptions.ConnectionError:
+        print "Error de conexion",chapter_url,", "
+        j+=1
+    max_num_chapter += 1
+
+
+sleep(5)
+raw_input("Press enter to concatenate "+str(max_num_chapter)+" pdf files")
+
+concatenate_pdf("97 cosas que todo programador", max_num_chapter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
